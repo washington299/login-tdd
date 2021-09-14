c83b92cda5fc9304ec925faa20130f169dac6ab5
Washington Campos <washingtoncampos9@gmail.com>
2021-09-14 00:22:44 -0300

    add font-family and size variables to global styles

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -7,11 +7,23 @@ export default createGlobalStyle`
         box-sizing: border-box;
     }
 
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+
     :root {
         --black: #000000;
         --white: #ffffff;
+        --gray: #cccccc;
+        --red: #f00000;
 
         --primary-color: #eeeeee;
         --secondary-color: #448BCB;
+
+        --extra-small-size: 0.5rem;
+        --small-size: 1rem;
+        --medium-size: 1.5rem;
+        --large-size: 2rem;
+        --extra-large-size: 2.5rem;
     }
 `;
